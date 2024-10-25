b18d1abfb04825ed387549695b4c3e1257881208
wicked-aashcharya <aashcharyakumargupta@gmail.com>
2024-10-26 00:32:26 +0530

    new

--- server/middleware/setCourseId.js
+++
@@ -1,8 +0,0 @@
-
-const setCourseId = (req, res, next) => {
-  const storedCourseId = localStorage.getItem("courseTodo"); 
-  req.courseId = storedCourseId ? JSON.parse(storedCourseId) : null; 
-  next();
-}
-
-module.exports = setCourseId;
